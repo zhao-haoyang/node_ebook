07690616df100811e30a085ab2a60374e3ed10d0
Ming-Rui-Zhang <1299722697@qq.com>
2021-03-09 18:43:40 +0800

    2改

---
+++ const.js
@@ -0,0 +1,34 @@
+const resUrl = 'http://10.6.51.183:801'
+const mp3FilePath = `E:/serve/mp3`
+
+const category = [
+  'ComputerScience',
+  'SocialSciences',
+  'Economics',
+  'Education',
+  'Engineering',
+  'Environment',
+  'Geography',
+  'History',
+  'Laws',
+  'LifeSciences',
+  'Literature',
+  'Biomedicine',
+  'BusinessandManagement',
+  'EarthSciences',
+  'MaterialsScience',
+  'Mathematics',
+  'MedicineAndPublicHealth',
+  'Philosophy',
+  'Physics',
+  'PoliticalScienceAndInternationalRelations',
+  'Psychology',
+  'Statistics'
+]
+
+
+module.exports = {
+  resUrl,
+  category,
+  mp3FilePath
+}
